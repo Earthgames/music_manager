19cb83f55a0b840eec19e5f183409cf1ced3313e
Earthgames <earthgame_s@outlook.com>
2024-02-21 23:21:41 +0100

    cleanup in category description

--- src/category_description.rs
+++ src/category_description.rs
@@ -1,5 +1,4 @@
-use crate::create_file;
-use crate::Result;
+use crate::{create_file, Result};
 use serde::{Deserialize, Serialize};
 use std::{fs, io::Error, path::Path};
 
@@ -25,16 +24,15 @@ pub fn create_category_description(
     category_path: &Path,
     category_name: Option<&str>,
     category_description: Option<&str>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // implement Path everywhere, makes things easier
-    // create path form the category_path, for now
-    let path_str = category_path.join("description.toml");
-    let path = Path::new(&path_str);
+) -> Result<()> {
+    // Create path form the category_path
+    let description_path = category_path.join("description.toml");
+
 
     let name = category_name.unwrap_or("default_name");
 
     let description = category_description
-        .unwrap_or("This is a default description for a category. Please add your own");
+        .unwrap_or("This is a default description for a category, please add your own");
 
     let content = Description {
         name: name.to_string(),
@@ -43,6 +41,6 @@ pub fn create_category_description(
 
     let toml = toml::to_string(&content)?;
 
-    create_file(path, toml)?;
+    create_file(&description_path, toml)?;
     Ok(())
 }
