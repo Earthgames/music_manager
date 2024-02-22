35f373c3a8ee705fd7284010cfdd44dd068c1238
Earthgames <earthgame_s@outlook.com>
2024-02-22 14:59:54 +0100

    now puts music files in subdirectories based on artist and album

--- src/commands.rs
+++ src/commands.rs
@@ -2,9 +2,16 @@ pub mod add;
 pub mod category;
 pub mod download;
 
-use crate::{category_description, config, move_files, read_dir, search_category, Result};
+use crate::{
+    category_description, config, move_file, music_tag::get_music_tag, read_dir, search, Result,
+};
 use log::{error, info, warn};
-use std::{fs, io::ErrorKind, path::Path, process};
+use std::{
+    fs,
+    io::ErrorKind,
+    path::{Path, PathBuf},
+    process,
+};
 
 // TODO: remove
 pub fn clean_tmp() {
@@ -88,5 +95,79 @@ pub fn move_to_category(category: &str, files: &Vec<String>) -> Result<()> {
         }
     };
 
-    move_files(files, &category_dir)
+    for file in files {
+        let file = PathBuf::from(file);
+
+        // get music tags
+        let music_tag = match get_music_tag(&file) {
+            Ok(tag) => tag,
+            Err(err) => {
+                warn!("could not find music tag because {err}");
+
+                // try the untagged directory
+                let untagged_dir = category_dir.join("Untagged");
+                if untagged_dir.is_dir() {
+                    match move_file(&file, &untagged_dir) {
+                        Ok(_) => info!(
+                            "moved {} to Untagged directory in {}",
+                            file.display(),
+                            category_dir.display()
+                        ),
+
+                        // if we can't move a file we won't try the rest
+                        Err(err) => {
+                            error!(
+                                "could not move {} to Untagged directory because {err}",
+                                file.display()
+                            );
+                            return Err(err);
+                        }
+                    };
+                }
+                continue;
+            }
+        };
+
+        // create artist and album directories if they do not exist
+        let artist_dir = category_dir.join(change_forbidden_chars(&music_tag.artist_name));
+        if !artist_dir.is_dir() {
+            //if we can't create a directory we won't try the rest
+            fs::create_dir(&artist_dir)?;
+            info!("Created {} artist directory", artist_dir.display());
+        }
+        let album_dir = artist_dir.join(change_forbidden_chars(&music_tag.album_title));
+        if !album_dir.is_dir() {
+            fs::create_dir(&album_dir)?;
+            info!("Created {} album directory", album_dir.display());
+        }
+
+        move_file(&file, &album_dir)?;
+        info!("Moved {} to {}", file.display(), album_dir.display());
+    }
+
+    Ok(())
+}
+
+fn change_forbidden_chars(input: &str) -> String {
+    let mut output = String::new();
+    for char in input.chars() {
+        // replace chars with chars that look like it
+        let char = match char {
+            '/' => '⁄',
+            '>' => '＞',
+            '<' => '＜',
+            '"' => {
+                output.push('\'');
+                '\''
+            } // we replace " with ''
+            '\\' => '＼',
+            '|' => '｜',
+            '?' => '？',
+            '*' => '＊',
+            ':' => '：',
+            _ => char,
+        };
+        output.push(char);
+    }
+    output
 }
